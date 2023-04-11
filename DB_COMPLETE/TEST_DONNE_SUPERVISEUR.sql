8e1b6497e99dd47c35bdfaaff457bbbf20ad3a44
Sofiane.T <sofkha123@gmail.com>
2023-04-11 13:18:50 +0200

    Update TEST_DONNE_SUPERVISEUR.sql

--- DB_COMPLETE/TEST_DONNE_SUPERVISEUR.sql
+++ DB_COMPLETE/TEST_DONNE_SUPERVISEUR.sql
@@ -1,2 +1,2 @@
 INSERT INTO do_user (nom,prenom,etablissement_user,ville,id_classe,email,password,category,active,activation_code,activation_expiry) 
-VALUES ('Monsieur','lesuper','YESSIR','Florida',NULL,'testadmin@gmail.com','testadmin','superviseur',1,'testadminnmdp','2007-04-30 13:10:02.0474381');
+VALUES ('Monsieur','lesuper','YESSIR','Florida',NULL,'testadmin@gmail.com','testadmin','superviseur',1,'zizizicaca','2007-04-30 13:10:02.0474381');
